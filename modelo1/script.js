81c6ca49f52eebb9497466b5d866dfb2b6ead78c
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2024-03-11 22:38:04 -0300

    Primeiro Site de Inspiração para nós

---
+++ modelo1/script.js
@@ -0,0 +1,12 @@
+// Get SideMenu
+var sideMenu = document.getElementById("sidemenu");
+
+// On openTab function call
+function openMenu() {
+  sideMenu.style.right = "0";
+}
+
+// On closeTab function call
+function closeMenu() {
+  sideMenu.style.right = "-220px";
+}
