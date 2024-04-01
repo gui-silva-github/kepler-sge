b5a84b5867dbd457fed81c1e12e8bd6194a3e962
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2024-03-31 23:16:25 -0300

    Scroll Reveal da Home Kepler

--- js/script.js
+++ js/script.js
@@ -10,3 +10,24 @@ function openMenu() {
 function closeMenu() {
   sideMenu.style.right = "-220px";
 }
+
+// Scroll Reveal's site
+
+ScrollReveal({ 
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+});
+
+ScrollReveal().reveal('.header-bottom, .head-title, .footer-top', { origin: 'top' });
+ScrollReveal().reveal('.opacity-layer, .flex-cards, .notice-headings, .iframe', { origin: 'bottom' });
+ScrollReveal().reveal('#cta-section, .notice, .hero-left', { origin: 'left' });
+ScrollReveal().reveal('.social-icons, .menu, .hero-right', { origin: 'right' });
+
+const typed = new Typed('.multiple-text', {
+  strings: ['FrontEnd Developer', 'Youtuber', 'Blogger'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
