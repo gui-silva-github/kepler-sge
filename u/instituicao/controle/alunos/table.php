70194df59c71dc52e1ee42b5fa7ff46ba666fdd3
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2024-05-02 15:23:44 -0300

    Ajustando src do jquery

--- u/instituicao/controle/alunos/table.php
+++ u/instituicao/controle/alunos/table.php
@@ -145,7 +145,7 @@
      <!-- Option 1: Bootstrap Bundle with Popper -->
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-    <script src="../../js/jquery/jquery.js"></script>
+    <script src="../../js/jquery.js"></script>
     <script src="//cdn.datatables.net/2.0.5/js/dataTables.min.js"></script>
 
     <script>
