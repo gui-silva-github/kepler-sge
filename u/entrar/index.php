16e1b67ba994edf5b770657da34959bab4ae50c3
MathAff <mathafensor00@gmail.com>
2024-05-15 22:39:10 -0300

    Favicon na tela de login
    Adicionado o Favicon na tela de login dos usuários

--- u/entrar/index.php
+++ u/entrar/index.php
@@ -70,6 +70,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kepler | Entrar</title>
     <link rel="stylesheet" href="style.css">
+    <!--Favicon-->
+    <link rel="shortcut icon" href="assets\favicon.png" type="image/x-icon">
     <!-- Boxicons CDN -->
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
